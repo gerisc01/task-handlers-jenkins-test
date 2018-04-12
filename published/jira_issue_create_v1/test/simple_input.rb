629c1f9dfc576a7776192e4edc6c7bbe107e2a95
Scott Gerike <gerisc01@luther.edu>
2018-04-12 11:43:24 -0500

    Add a jira issue create handler

---
+++ published/jira_issue_create_v1/test/simple_input.rb
@@ -0,0 +1,17 @@
+{
+  'info' => {
+    'site' => '',
+    'username' => '',
+    'password' => '',
+    'enable_debug_logging' => 'Yes'
+  },
+  'parameters' => {
+    'summary' => 'ISSUE TITLE',
+    'project_key' => '',
+    'type' => 'New Feature',
+    'priority' => '',
+    'assignee' => '',
+    'due_date' => 'YYYY-MM-DD',
+    'description' => 'ISSUE DESCRIPTION'
+  }
+}
